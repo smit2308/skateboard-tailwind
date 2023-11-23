b1e319e2e6cc28aa38730312ddd2c991ca1b775c
smit2308 <62828738+smit2308@users.noreply.github.com>
2023-11-23 00:46:44 -0800

    updated images in offer

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
-import nobg_element from '../assets/images/banner/nobg_element2.png'
+import nobg_element from '../assets/images/banner/nobg_element1.png'
+import nobg_element3 from '../assets/images/banner/nobg_element3.png'
+import nobg_giraffe from '../assets/images/banner/nobg_giraffe1.png'
+import nobg_element2 from '../assets/images/banner/nobg_element2.png'
+import nobg_element4 from '../assets/images/banner/nobg_element4.png'
+import nobg_element5 from '../assets/images/banner/nobg_element5.png'
+import nobg_element6 from '../assets/images/banner/nobg_element6.png'
 import Button from '../components/Button'
 import { arrowRight } from '../assets/icons'
 
 const SpecialOffer = () => {
   return (
     <section className='flex justify-center '>
-    <div className=' lg:w-[800px]   lg:h-[500px] shadow-2xl rounded-3xl flex lg:flex-row flex-col overflow-clip  '>
+    <div className=' lg:w-[900px]   lg:h-[500px] shadow-2xl rounded-3xl flex lg:flex-row flex-col overflow-clip  '>
 
       <div className=' bg-gray-100  flex overflow-hidden justify-center max-lg:h-[400px]'>
         <div className='  flex -rotate-[30deg] object-none max-lg:scale-150'>
           <div className='  flex flex-col gap-6 -ml-16'>
-            <img src={nobg_element} width={240}  alt="nobg_element" />
-            <img src={nobg_element} width={240}  alt="nobg_element" />
+            <img src={nobg_element6} width={240}  alt="nobg_element" />
+            <img src={nobg_element2} width={240}  alt="nobg_element" />
             
           </div>
           <div className='   flex flex-col gap-6 -ml-24 -mt-36'>
+            <img src={nobg_element3} width={240}  alt="nobg_element" />
             <img src={nobg_element} width={240}  alt="nobg_element" />
-            <img src={nobg_element} width={240}  alt="nobg_element" />
-            <img src={nobg_element} width={240}  alt="nobg_element" />
+            <img src={nobg_element3} width={240}  alt="nobg_element" />
           </div>
           <div className='  flex flex-col gap-6 -ml-24'>
-            <img src={nobg_element} width={240}  alt="nobg_element" />
-            <img src={nobg_element} width={240}  alt="nobg_element" />
+            <img src={nobg_element5} width={240}  alt="nobg_element" />
+            <img src={nobg_element4} width={240}  alt="nobg_element" />
             
           </div>
+          
           
           
 
