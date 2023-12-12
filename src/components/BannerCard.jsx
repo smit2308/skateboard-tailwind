13d3582aeaf08015d650cc3cb50e1c5826b6c941
smit2308 <62828738+smit2308@users.noreply.github.com>
2023-12-11 23:37:56 -0800

    testing vite yml deploys
    changed vite.config.js to ts
    added .github folder

--- src/components/BannerCard.jsx
+++ src/components/BannerCard.jsx
@@ -1,10 +1,19 @@
 import React from "react"
 import BlankPoster from  '../assets/images/blank_poster.png'
-
+import { Link } from "react-router-dom"
+import { data } from "autoprefixer"
 
 const BannerCard=({skate}) => {
+
+  // data = {
+  //   imgURL:skate.imgURL,
+  //   name:skate.name,
+  //   price:skate.price,
+  //   rating:skate.rating
+  // }
     
     return(
+      <Link   to={`/products/${skate.name}`} state={{skate}}> 
         <div className='w-[200px] h-[400px] max-sm:w-[140px] max-sm:h-[300px] xl:w-60 xl:h-[500px]  overflow-hidden bg-neutral-100  transition-all duration-[0.1s] ease-[ease-in-out] shadow-lg
         p-4 rounded-[30px]  hover:shadow-2xl hover:scale-105  '>
 
@@ -28,6 +37,7 @@ const BannerCard=({skate}) => {
       
 
       </div>
+      </Link>
     )
 
 }
